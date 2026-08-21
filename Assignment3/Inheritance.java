bba1b65d18ef93731187c41d9aba4ac939715b37
Snehalahu <144317521+Snehalahu@users.noreply.github.com>
2026-08-21 21:18:41 +0530

    Create Inheritance.java

---
+++ Assignment3/Inheritance.java
@@ -0,0 +1,27 @@
+class Employee {
+
+    void work() {
+        System.out.println("Employee is working.");
+    }
+}
+
+class Developer extends Employee {
+
+    void writeCode() {
+        System.out.println("Developer is writing Java code.");
+    }
+}
+
+public class Inheritance {
+
+    public static void main(String[] args) {
+
+        Developer dev = new Developer();
+
+        // Inherited method
+        dev.work();
+
+        // Developer's own method
+        dev.writeCode();
+    }
+}
